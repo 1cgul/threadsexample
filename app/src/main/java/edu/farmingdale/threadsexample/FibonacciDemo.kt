e861a876457dc728e3402234cc04b442fffbd5de
1cgul <cgulino03@gmail.com>
2024-11-19 15:14:32 -0500

    added padding

--- app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/FibonacciDemo.kt
@@ -2,6 +2,8 @@ package edu.farmingdale.threadsexample
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -12,7 +14,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.text.NumberFormat
@@ -23,7 +27,11 @@ fun FibonacciDemoNoBgThrd() {
     var answer by remember { mutableStateOf("") }
     var textInput by remember { mutableStateOf("40") }
 
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 32.dp)
+    ) {
         Row {
             TextField(
                 value = textInput,
@@ -50,3 +58,14 @@ fun FibonacciDemoNoBgThrd() {
 fun fibonacci(n: Long): Long {
     return if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
 }
+
+
+// ToDo 2: Create a composable function called `FibonacciDemoWithCoroutine` that calculates the
+//  Fibonacci number of a given number using a coroutine.
+// ToDo 3: Start the application using the CountDownActivity
+// ToDo 4: Make the Text of the timer larger
+// ToDo 5: Show a visual indicator of the timer going down to 0
+// ToDo 6: Add a button to rest the timer
+// ToDo 7: Play a sound when the timer reaches 0
+// ToDo 8: During the last 10 seconds, make the text red and bold
+
--- app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
@@ -19,9 +19,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ThreadsExampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting("Android", Modifier.padding(innerPadding))
-                }
+                FibonacciDemoNoBgThrd()
             }
         }
     }
@@ -34,13 +32,3 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
-
-// ToDo 1: Call `FibonacciDemoNoBgThrd` that calculates the Fibonacci number of a given number.
-// ToDo 2: Create a composable function called `FibonacciDemoWithCoroutine` that calculates the
-//  Fibonacci number of a given number using a coroutine.
-// ToDo 3: Start the application using the CountDownActivity
-// ToDo 4: Make the Text of the timer larger
-// ToDo 5: Show a visual indicator of the timer going down to 0
-// ToDo 6: Add a button to rest the timer
-// ToDo 7: Play a sound when the timer reaches 0
-// ToDo 8: During the last 10 seconds, make the text red and bold
